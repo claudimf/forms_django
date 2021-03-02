948e38eba02bb3daf35b61692404ec33665e84ee
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-02 17:02:30 -0300

    Makes class "Exibindo dados"

--- passagens/views.py
+++ passagens/views.py
@@ -6,3 +6,10 @@ def index(request):
     form = PassagemForms()
     context = {'form': form}
     return render(request, 'index.html', context)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+
+        context = {'form': form}
+        return render(request, 'minha_consulta.html', context)
