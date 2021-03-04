171544adaba94d42902bac54b31317bc90031d03
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-04 16:37:41 -0300

    Makes course class "Validando datas"

--- passagens/validation.py
+++ passagens/validation.py
@@ -8,3 +8,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """Verifica se há algum número no campo"""
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = str(nome_campo) + ': Não inclua números'
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """Verifica se a data de ida é maior que a data de volta"""
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser menor que a de ida'
+
+def data_ida_menor_que_data_de_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """Verifica se a data de ida é menor que data de hoje"""
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de volta não pode ser menor que hoje'
